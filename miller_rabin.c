ea14991ad1016fe37b6849baa623c0f4af0d764a
Andrew Gibbons <andrew.h.gibbons@protonmail.com>
2019-06-10 01:07:47 +0900

    Initial Commit.

---
+++ miller_rabin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
+
+bool mr_test(int n, int numtests, int seed) {
+	bool isprime = true;
+	for (int i = 0; i < numtests; ++i)
+	{
+		
+	}:
+
+	return false;
+}
